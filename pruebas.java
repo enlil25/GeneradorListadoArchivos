1e3ce1cdff94431a870d2cddf8edd233437ec2ee
cesar alberto <devjav.cc@gmail.com>
2015-07-27 23:10:59 -0500

    archivo de pruebas creado

---
+++ pruebas.java
@@ -0,0 +1,17 @@
+public class pruebas{
+	
+	private String nombre;
+	
+	public pruebas(String nombre){
+		this.nombre=nombre;
+	}
+	
+	public String getNombre(){
+		return this.nombre;
+	}
+	
+	public void setNombre(String nombre){
+		this.nombre=nombre;
+	}
+	
+}
